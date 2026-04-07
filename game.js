db13a722ac3382de8d6d26402f7d343c2d63ba06
allenres <allen.resulidze@gmail.com>
2026-04-07 11:22:33 -0700

    add upgrades array

--- game.js
+++ game.js
@@ -1,6 +1,10 @@
 let score = 0;
 let pointsPerClick = 1;
 
+const upgrades = [
+    {id: 1, name: "Large Watering Can", cost: 3, bonus: 4},
+    {id: 2, name: "Medium Watering Can", cost: 1, bonus: 2}
+]
 
 const updateDisplay = () => {
     const scoreElement = document.getElementById("score-display");
@@ -16,3 +20,6 @@ clickButton.addEventListener("click", () => {
     score += pointsPerClick;
     updateDisplay();
 });
+
+// const uprgadesSection = document.getElementById("upgrades");
+// uprgadesSection.innerHTML += upgrades.forEach(e => `<p>${e.id}</p>`);
