d6015ca4ca5ab512d033ef2abd40778a3b1f9e0c
Akaioka <brendanvillaraza@yahoo.com>
2026-04-07 11:04:24 -0700

    Added functions for Track the Score section

--- game.js
+++ game.js
@@ -1,2 +1,18 @@
 let score = 0;
 let pointsPerClick = 1;
+
+
+const updateDisplay = () => {
+    const scoreElement = document.getElementById("score-display");
+    const rateElement = document.getElementById("rate-display")
+
+    scoreElement.textContent = 'Height: ' + score;
+    rateElement.textContent = 'Inches per click: ' + pointsPerClick;
+}
+
+const clickButton = document.getElementById("click-btn");
+
+clickButton.addEventListener("click", () => {
+    score += pointsPerClick;
+    updateDisplay();
+});
