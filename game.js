2089d426ab59a11c6e79c84cecf552765bdda70e
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-04-07 16:51:49 -0700

    Added feature Track how many times an upgrade has been purchased

--- game.js
+++ game.js
@@ -2,9 +2,9 @@ let score = 0;
 let pointsPerClick = 1;
 
 const upgrades = [
-    {id: 1, name: "Large Watering Can", cost: 3, bonus: 4},
-    {id: 2, name: "Medium Watering Can", cost: 1, bonus: 2},
-    {id: 3, name: "Garden Hose", cost: 1000, bonus: 100},
+    {id: 1, name: "Large Watering Can", cost: 3, bonus: 4, timesBought: 0},
+    {id: 2, name: "Medium Watering Can", cost: 1, bonus: 2, timesBought: 0},
+    {id: 3, name: "Garden Hose", cost: 1000, bonus: 100, timesBought: 0},
 ]
 
 const updateDisplay = () => {
@@ -31,6 +31,7 @@ const renderUpgrades = () => {
     upgrades.forEach(el => {
         const upgradeElement = document.createElement("div");
         const button = document.createElement("button");
+        
 
         button.textContent = "Buy Upgrade";
         button.onclick = () => buyUpgrade(el.id);
@@ -42,7 +43,8 @@ const renderUpgrades = () => {
         upgradeElement.innerHTML = `
             <p>${el.name}</p>
             <p>cost: $${el.cost}</p>
-            <p>Bonus growth: ${el.bonus}+</p>`;
+            <p>Bonus growth: ${el.bonus}+</p>
+            <p>Times bought: ${el.timesBought}</p>`;
 
         upgradeElement.appendChild(button);
         upgradesSection.appendChild(upgradeElement);
@@ -56,11 +58,14 @@ const buyUpgrade = (id) => {
     if(score >= upgrade.cost) {
         score -= upgrade.cost;
         pointsPerClick += upgrade.bonus;
+        //Track how many times each upgrade has been purchased and show count
+        upgrade.timesBought += 1;
         updateDisplay();
         renderUpgrades();
     }  
 }
 
+// Auto increment
 const pointIncrement = () => {
     score += 1;
     updateDisplay();
@@ -68,3 +73,5 @@ const pointIncrement = () => {
 }
 
 setInterval(pointIncrement, 1000);
+
+
